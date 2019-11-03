bf815fe96ebb60a02a2048601f0584f28cfcc581
Denis <dendiesel88@yandex.ru>
2019-11-03 18:20:20 +0300

    version 2

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> todoList = new ArrayList<>();
+        String[] taskText;
+        int number = 0;
 
         for (; ; ) {
             String text = scanner.nextLine();
-            System.out.println(text.matches("^[A-Z]+ \\d+ .+"));
-            String[] taskWithNumber = text.split("^[A-Z]+ \\d+ ");
-            String[] taskNoNumber = text.split("^[A-Z]+ ");
             String[] subTask = text.split(" ");
 
+            if (text.matches("^[A-Z]+ \\d+ .+")) {
+                taskText = text.split("^[A-Z]+ \\d+ ");
+                number = Integer.parseInt(subTask[1]);
+            }
+            else {
+                taskText= text.split("^[A-Z]+ ");
+            }
+//LIST
             if (subTask[0].equals("LIST")) {
                 for (String word : todoList) {
                     System.out.println(word);
                 }
             }
-
-            if (subTask[0].equals("ADD") && taskWithNumber.length == 2) {
-                if (Integer.parseInt(subTask[1]) > todoList.size()) {
+//ADD
+            if (subTask[0].equals("ADD") && taskText.length == 2) {
+                if (number > todoList.size()) {
                     System.out.println("Введите номер задания меньше чем " + (todoList.size()+1));
                 } else {
-                    todoList.add(Integer.parseInt(subTask[1]), taskWithNumber[1]);
+                    todoList.add(number, taskText[1]);
                 }
-            } else if (subTask[0].equals("ADD") && taskWithNumber.length == 1) {
-                todoList.add(taskNoNumber[1]);
+            } else if (subTask[0].equals("ADD") && taskText.length == 1) {
+                todoList.add(taskText[1]);
             }
-
+//EDIT
             if (subTask[0].equals("EDIT")) {
-                if (Integer.parseInt(subTask[1])> todoList.size()) {
+                if (number> todoList.size()) {
                     System.out.println("Введите номер задания меньше чем " + (todoList.size()+1));
                 } else {
-                    todoList.set(Integer.parseInt(subTask[1]), taskWithNumber[1]);
+                    todoList.set(number, taskText[1]);
                 }
             }
+//DELETE
             if (subTask[0].equals("DELETE")) {
-                if (Integer.parseInt(subTask[1]) > todoList.size()) {
+                if (number > todoList.size()) {
                     System.out.println("Введите номер задания меньше чем " + (todoList.size()+1));
                 } else {
-                    todoList.remove(Integer.parseInt(subTask[1]));
+                    todoList.remove(number);
                 }
             }
         }
